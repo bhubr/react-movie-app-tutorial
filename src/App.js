01dc20ef0fdfee7970f3fa21c8cbf38ab53537da
Benoit Hubert <benoithubert@gmail.com>
2020-12-14 08:48:57 +0100

    Create CSS container and use MovieCard component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
 import Header from "./components/Header";
+import MovieCard from "./components/MovieCard";
 
 function App() {
-  return <div className="App">
+  return <div className="App container">
     <Header title="Ma bibliothèque de films" />
+    <MovieCard />
+    <MovieCard />
+    <MovieCard />
+    <MovieCard />
+    <MovieCard />
+    <MovieCard />
   </div>;
 }
 
