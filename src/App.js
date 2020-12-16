005e8a1af3ab11ada4fe065ace8fed12df6f9450
Benoit Hubert <benoithubert@gmail.com>
2020-12-16 07:54:54 +0100

    Repeat MovieCard component from an array of objects (+ ul/li example)

--- src/App.js
+++ src/App.js
@@ -2,23 +2,49 @@ import "./App.css";
 import Header from "./components/Header";
 import MovieCard from "./components/MovieCard";
 
+const moviesArr = [
+  {
+    id: 1,
+    title: "Inception",
+    poster: "https://d2e111jq13me73.cloudfront.net/sites/default/files/styles/product_image_aspect_switcher_170w/public/product-images/csm-movie/inceptionmovie-newposter-big.jpg?itok=zn3rPAOq",
+    description: "Un film de S-F",
+  },
+  {
+    id: 2,
+    title: "Avatar",
+    poster: "https://i.pinimg.com/originals/1b/fc/63/1bfc633cd889e545f521ea14da1b8df9.jpg",
+    description: "Un film avec des aliens bleus",
+  },
+  {
+    id: 3,
+    title: "Interstellar",
+    poster: "https://upload.wikimedia.org/wikipedia/en/b/bc/Interstellar_film_poster.jpg",
+    description: "Un autre film de S-F"
+  }
+];
+
+const numbers = [1, 2, 3, 4, 5];
+
 function App() {
   return <div className="App container">
+    <ul>
+      {
+        numbers.map((number) =>
+          <li key={number}>{number}</li>
+        )
+      }
+    </ul>
     <Header title="Ma bibliothèque de films" />
-    <MovieCard
-      title="Inception"
-      poster="https://d2e111jq13me73.cloudfront.net/sites/default/files/styles/product_image_aspect_switcher_170w/public/product-images/csm-movie/inceptionmovie-newposter-big.jpg?itok=zn3rPAOq"
-      description="Un film de S-F"
-    />
-    <MovieCard
-      title="Avatar"
-      poster="https://i.pinimg.com/originals/1b/fc/63/1bfc633cd889e545f521ea14da1b8df9.jpg"
-      description="Un film avec des aliens bleus"
-    />
-    <MovieCard />
-    <MovieCard />
-    <MovieCard />
-    <MovieCard />
+    {
+      moviesArr.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          title={movie.title}
+          poster={movie.poster}
+          description={movie.description}
+        />
+      ))
+    }
   </div>;
 }
 
