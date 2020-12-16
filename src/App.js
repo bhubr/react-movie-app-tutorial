6b7f5e47b51c81574bcfad49dffede6c1f623a93
Benoit Hubert <benoithubert@gmail.com>
2020-12-16 07:35:36 +0100

    Add props to MovieCard component

--- src/App.js
+++ src/App.js
@@ -5,8 +5,16 @@ import MovieCard from "./components/MovieCard";
 function App() {
   return <div className="App container">
     <Header title="Ma bibliothèque de films" />
-    <MovieCard />
-    <MovieCard />
+    <MovieCard
+      title="Inception"
+      poster="https://d2e111jq13me73.cloudfront.net/sites/default/files/styles/product_image_aspect_switcher_170w/public/product-images/csm-movie/inceptionmovie-newposter-big.jpg?itok=zn3rPAOq"
+      description="Un film de S-F"
+    />
+    <MovieCard
+      title="Avatar"
+      poster="https://i.pinimg.com/originals/1b/fc/63/1bfc633cd889e545f521ea14da1b8df9.jpg"
+      description="Un film avec des aliens bleus"
+    />
     <MovieCard />
     <MovieCard />
     <MovieCard />
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,19 +1,29 @@
+import PropTypes from "prop-types";
 import "./MovieCard.css";
 
 function MovieCard(props) {
   return (
     <article className="MovieCard">
-      <img
-        className="MovieCard-poster"
-        src="https://d32qys9a6wm9no.cloudfront.net/images/movies/poster/500x735.png"
-        alt="Movie poster"
-      />
+      <img className="MovieCard-poster" src={props.poster} alt={props.title} />
       <div>
-        <h3>Movie title</h3>
-        <p>Movie description</p>
+        <h3>{props.title}</h3>
+        <p>{props.description}</p>
       </div>
     </article>
-  )
+  );
 }
+
+MovieCard.defaultProps = {
+  poster:
+    "https://d32qys9a6wm9no.cloudfront.net/images/movies/poster/500x735.png",
+  title: "Insert title",
+  description: "Insert description",
+};
+
+MovieCard.propTypes = {
+  title: PropTypes.string,
+  poster: PropTypes.string,
+  description: PropTypes.string,
+};
 
 export default MovieCard;
